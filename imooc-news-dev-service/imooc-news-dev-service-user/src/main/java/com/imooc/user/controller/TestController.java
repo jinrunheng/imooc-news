5272037caed6784fe05d3c5864d16f02430bec12
jinrunheng <1175088275@qq.com>
2021-12-19 14:36:36 +0800

    新增 lombok 的依赖，并做了日志打印的测试

--- imooc-news-dev-service/imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/TestController.java
+++ imooc-news-dev-service/imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/TestController.java
@@ -1,12 +1,15 @@
 package com.imooc.user.controller;
 
 import com.imooc.api.controller.user.TestControllerApi;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController implements TestControllerApi {
 
     public Object test() {
+        log.info("Test Lombok");
         return "hello";
     }
 }
