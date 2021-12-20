cdd64a4d913d30592e93652abde4bc9256c9a9bd
jinrunheng <1175088275@qq.com>
2021-12-20 15:47:51 +0800

    完成配置类 Swagger2Configuration；在 HelloControllerApi 中添加了 Swagger2 文档构建注解

---
+++ imooc-news-dev-service/imooc-news-dev-service-api/src/main/java/com/imooc/api/config/Swagger2Configuration.java
@@ -0,0 +1,48 @@
+package com.imooc.api.config;
+
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author Dooby Kim
+ * @Date 2021/12/20 3:08 下午
+ * @Version 1.0
+ * <p>
+ * Swagger2 文档路径地址：配置项目路径/doc.html
+ * 例如：http://user.imoocnews.com:8003/doc.html
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Configuration {
+
+    @Bean
+    public Docket createRestApi() {
+
+        Predicate<RequestHandler> userPredicate = RequestHandlerSelectors.basePackage("com.imooc.user.controller");
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(Predicates.or(userPredicate))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("imooc-news 接口 api")
+                .description("imooc-news 接口 api")
+                .version("1.0")
+                .build();
+    }
+}
--- imooc-news-dev-service/imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/HelloControllerApi.java
+++ imooc-news-dev-service/imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/HelloControllerApi.java
@@ -1,9 +1,13 @@
 package com.imooc.api.controller.user;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Api(value = "HelloController")
 public interface HelloControllerApi {
 
+    @ApiOperation(value = "hello 方法", httpMethod = "GET")
     @GetMapping("/hello")
     Object hello();
 }
