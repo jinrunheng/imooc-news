2f50dda201891729d020618d5e86ca594b3e87b9
jinrunheng <1175088275@qq.com>
2021-12-23 11:42:07 +0800

    完成 IPUtils 工具类，用于获取用户真实的 ip 地址

--- imooc-news-dev-service/imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/PassportControllerApi.java
+++ imooc-news-dev-service/imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/PassportControllerApi.java
@@ -5,15 +5,18 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @Author Dooby Kim
  * @Date 2021/12/22 3:18 下午
  * @Version 1.0
+ * @Description 用户通行证 Controller 接口
  */
 @Api(value = "PassportController", tags = {"用户注册登录通行证"})
 public interface PassportControllerApi {
 
     @ApiOperation(value = "获取短信验证码", notes = "获取短信验证码", httpMethod = "GET")
     @GetMapping("/getSMSCode")
-    JsonResult getSMSCode();
+    JsonResult getSMSCode(String phone, HttpServletRequest request);
 }
--- imooc-news-dev-service/imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/PassportController.java
+++ imooc-news-dev-service/imooc-news-dev-service-user/src/main/java/com/imooc/user/controller/PassportController.java
@@ -2,11 +2,14 @@ package com.imooc.user.controller;
 
 import com.imooc.api.controller.user.PassportControllerApi;
 import com.imooc.result.JsonResult;
+import com.imooc.utils.RedisOperator;
 import com.imooc.utils.SMSUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
@@ -16,13 +19,18 @@ import java.util.Map;
  */
 @RestController
 @Slf4j
+@RequestMapping("passport")
 public class PassportController implements PassportControllerApi {
 
     @Resource
     private SMSUtils smsUtils;
 
+    @Resource
+    private RedisOperator redisOperator;
+
     @Override
-    public JsonResult getSMSCode() {
+    public JsonResult getSMSCode(String phone, HttpServletRequest request) {
+        // HttpServletRequest 可以获取到用户的 ip；根据用户的 ip 地址进行限制，限制用户在 60 秒内只能获取一次验证码
         // 随机生成四位数字的验证码
         String code = smsUtils.generateSMSCode();
         Map<String, String> result = smsUtils.sendSMS("15526787357", code);
