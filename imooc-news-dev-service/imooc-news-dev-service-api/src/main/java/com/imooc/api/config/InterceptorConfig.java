5f3edb44d58e08050aefca095c79715cfdd6e486
jinrunheng <1175088275@qq.com>
2021-12-23 15:36:50 +0800

    新增并配置拦截器 PassportInterceptor 用于拦截对 /passport/getSMSCode 的请求；方法是通过 redis 来查看同一个用户的 ip 是否存在，如果存在说明该用户在 60 秒以内重复点击发送验证码

---
+++ imooc-news-dev-service/imooc-news-dev-service-api/src/main/java/com/imooc/api/config/InterceptorConfig.java
@@ -0,0 +1,28 @@
+package com.imooc.api.config;
+
+import com.imooc.api.interceptor.PassportInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author Dooby Kim
+ * @Date 2021/12/23 3:28 下午
+ * @Version 1.0
+ * @Description 拦截器相关配置
+ */
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Bean
+    public PassportInterceptor passportInterceptor() {
+        return new PassportInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(passportInterceptor())
+                .addPathPatterns("/passport/getSMSCode");
+    }
+}
---
+++ imooc-news-dev-service/imooc-news-dev-service-api/src/main/java/com/imooc/api/interceptor/PassportInterceptor.java
@@ -0,0 +1,73 @@
+package com.imooc.api.interceptor;
+
+import com.imooc.utils.IPUtils;
+import com.imooc.utils.RedisKeyUtils;
+import com.imooc.utils.RedisOperator;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Author Dooby Kim
+ * @Date 2021/12/23 3:16 下午
+ * @Version 1.0
+ * @Description 通行证服务相关拦截器
+ */
+public class PassportInterceptor implements HandlerInterceptor {
+
+    @Resource
+    private RedisOperator redisOperator;
+
+    /**
+     * 拦截器处理，在访问 Controller 以前
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return 如果返回 true 表示并未拦截；如果返回 false 表示被拦截
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String ip = IPUtils.getIPAddress(request);
+        boolean keyIsExist = redisOperator.hasKey(RedisKeyUtils.userIpKey(ip));
+        if (keyIsExist) {
+            // 如果 key 存在，说明用户获取验证码时间还没有超过 60 秒
+            // TODO
+            System.out.println("短信发送频率过高");
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 拦截起处理，在请求访问到 Controller 以后，渲染视图以前
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    /**
+     * 拦截起处理，渲染视图以后
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
