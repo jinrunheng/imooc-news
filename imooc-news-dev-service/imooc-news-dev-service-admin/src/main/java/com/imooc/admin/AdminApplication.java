86a54b1a8714398728f29c6f57aafec8e1cb3a7c
jinrunheng <1175088275@qq.com>
2022-01-04 17:57:12 +0800

    创建 admin 模块，完成 /hello 接口测试

---
+++ imooc-news-dev-service/imooc-news-dev-service-admin/src/main/java/com/imooc/admin/AdminApplication.java
@@ -0,0 +1,21 @@
+package com.imooc.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * @Author Dooby Kim
+ * @Date 2022/1/4 5:47 下午
+ * @Version 1.0
+ */
+@SpringBootApplication
+@MapperScan(basePackages = "com.imooc.admin.mapper")
+@ComponentScan(basePackages = {"com.imooc"})
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
+
---
+++ imooc-news-dev-service/imooc-news-dev-service-admin/src/main/java/com/imooc/admin/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.imooc.admin.controller;
+
+import com.imooc.api.controller.user.HelloControllerApi;
+import com.imooc.result.JsonResult;
+import com.imooc.utils.RedisOperator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @Author Dooby Kim
+ * @Date 2022/1/4 5:50 下午
+ * @Version 1.0
+ */
+@RestController
+@Slf4j
+public class HelloController implements HelloControllerApi {
+
+    public JsonResult hello() {
+        log.info("Test Hello");
+        return JsonResult.ok();
+    }
+}
