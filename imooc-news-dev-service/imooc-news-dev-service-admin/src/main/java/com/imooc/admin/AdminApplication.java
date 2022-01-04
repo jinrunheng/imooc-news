a1757bb749785b0c6dd0197a861723b6712f4cd8
jinrunheng <1175088275@qq.com>
2022-01-04 17:57:12 +0800

    创建 admin 模块，完成基本的配置，完成 /hello 的接口测试

---
+++ imooc-news-dev-service/imooc-news-dev-service-admin/src/main/java/com/imooc/admin/AdminApplication.java
@@ -0,0 +1,9 @@
+package com.imooc.admin;
+
+/**
+ * @Author Dooby Kim
+ * @Date 2022/1/4 5:47 下午
+ * @Version 1.0
+ */
+public class AdminApplication {
+}
---
+++ imooc-news-dev-service/imooc-news-dev-service-admin/src/main/java/com/imooc/admin/controller/HelloController.java
@@ -0,0 +1,9 @@
+package com.imooc.admin.controller;
+
+/**
+ * @Author Dooby Kim
+ * @Date 2022/1/4 5:50 下午
+ * @Version 1.0
+ */
+public class HelloController {
+}
