e75c20b4cbbdd21d5bc936924b2f95dec1c97387
jinrunheng <1175088275@qq.com>
2022-01-05 23:09:34 +0800

    解决冲突

--- imooc-news-dev-service/imooc-news-dev-service-admin/src/main/java/com/imooc/admin/AdminApplication.java
+++ imooc-news-dev-service/imooc-news-dev-service-admin/src/main/java/com/imooc/admin/AdminApplication.java
@@ -1,19 +1,16 @@
 package com.imooc.admin;
 
-<<<<<<< HEAD
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import tk.mybatis.spring.annotation.MapperScan;
 
-=======
->>>>>>> a1757bb749785b0c6dd0197a861723b6712f4cd8
 /**
  * @Author Dooby Kim
  * @Date 2022/1/4 5:47 下午
  * @Version 1.0
  */
-<<<<<<< HEAD
+
 @SpringBootApplication
 @MapperScan(basePackages = "com.imooc.admin.mapper")
 @ComponentScan(basePackages = {"com.imooc"})
@@ -23,7 +20,4 @@ public class AdminApplication {
     }
 }
 
-=======
-public class AdminApplication {
-}
->>>>>>> a1757bb749785b0c6dd0197a861723b6712f4cd8
+
